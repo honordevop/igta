1a37b6942b61307e34f902a9c875934a9620fdbd
Ogunlade Stephen O <ogunladestephen20@gmail.com>
2024-05-11 12:52:21 +0100

    Code: Adjusted Hero component view height

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import { IoMdCheckmarkCircleOutline } from "react-icons/io";
 
 const Hero = () => {
   return (
-    <div className="w-full flex flex-col md:flex-row  h-[80vh] ">
+    <div className="w-full flex flex-col md:flex-row  h-[90vh] ">
       <div className="flex-1 flex flex-col gap-7 mt-[30px] md:mt-[60px] ">
         <p className="primaryBgColor px-4 py-1 rounded-2xl text-white w-max font-semibold tracking-[1.5px]">
           IGTA RN: 7312655
