ddf5aa10f014c45ae88c848b4d072baccc868912
Ogunlade Stephen O <ogunladestephen20@gmail.com>
2024-05-18 20:10:49 +0100

    Code: typoghraphy correction

--- src/app/events/page.jsx
+++ src/app/events/page.jsx
@@ -8,8 +8,8 @@ const Events = () => {
       <PageHeader title="Ongoing Events & Trainings Activities" />
 
       <div className="w-full my-20 text-center flex flex-col gap-4 items-center justify-center ">
-        <h3 className="text-4xl font-bold mb-12">
-          Outline below is our current and ongoing trainings
+        <h3 className="text-4xl font-bold mb-12 px-2">
+          Outline below are the current and ongoing trainings
         </h3>
 
         <Event />
