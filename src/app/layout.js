6b9c29068ae89b654270dd912ff099cf85b14d01
Ogunlade Stephen O <ogunladestephen20@gmail.com>
2024-06-16 23:55:47 +0100

    Analytics: implemented google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -33,48 +33,24 @@ export default function RootLayout({ children }) {
           href="https://fonts.googleapis.com/css2?family=Syne+Tactile&display=swap"
           rel="stylesheet"
         ></link>
-        <Script
-          type="text/javascript"
-          onLoad={() => {
-            (window.heap = window.heap || []),
-              (heap.load = function (e, t) {
-                (window.heap.appid = e), (window.heap.config = t = t || {});
-                var r = document.createElement("script");
-                (r.type = "text/javascript"),
-                  (r.async = !0),
-                  (r.src =
-                    "https://cdn.heapanalytics.com/js/heap-" + e + ".js");
-                var a = document.getElementsByTagName("script")[0];
-                a.parentNode.insertBefore(r, a);
-                for (
-                  var n = function (e) {
-                      return function () {
-                        heap.push(
-                          [e].concat(Array.prototype.slice.call(arguments, 0))
-                        );
-                      };
-                    },
-                    p = [
-                      "addEventProperties",
-                      "addUserProperties",
-                      "clearEventProperties",
-                      "identify",
-                      "resetIdentity",
-                      "removeEventProperty",
-                      "setEventProperties",
-                      "track",
-                      "unsetEventProperty",
-                    ],
-                    o = 0;
-                  o < p.length;
-                  o++
-                )
-                  heap[p[o]] = n(p[o]);
-              });
-            heap.load("1598158189");
-          }}
-        />
       </Head>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-TKXB4PVFST"
+      />
+      <Script strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-TKXB4PVFST');
+  `}
+      </Script>
+      {/* <Script type="text/javascript" strategy="afterInteractive">
+        {`window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
+  heap.load("1598158189");`}
+      </Script> */}
       <body className={`${inter.className} ${space_grotesk.className} `}>
         <AppProvider>
           <AuthProvider>
