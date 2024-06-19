4a363c776147108cfdbb27e6922e08f08d50e663
Ogunlade Stephen O <ogunladestephen20@gmail.com>
2024-06-19 15:58:13 +0100

    Code: Changed edit and delete btn to icon

--- src/components/EventsList.jsx
+++ src/components/EventsList.jsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import React, { useState } from "react";
 import UpdateEventForm from "./UpdateEventForm";
 import { BeatLoader } from "react-spinners";
+import { MdDeleteForever } from "react-icons/md";
+import { CiEdit } from "react-icons/ci";
 
 const EventsList = ({
   data,
@@ -40,7 +42,7 @@ const EventsList = ({
                   className="px-2 py-1 bg-green-600 hover:primaryBgVolorLight font-semibold text-lg text-white rounded-lg cursor-pointer"
                   onClick={() => showUpdateEventFormHandler(`${item._id}`)}
                 >
-                  Update
+                  <CiEdit />
                 </div>
 
                 <div
@@ -49,7 +51,11 @@ const EventsList = ({
                     deleteHandler(`${item._id}`);
                   }}
                 >
-                  {deleteLoading ? <BeatLoader color="#fff" /> : <p>Delete</p>}
+                  {deleteLoading ? (
+                    <BeatLoader color="#fff" />
+                  ) : (
+                    <MdDeleteForever />
+                  )}
                 </div>
               </td>
             </tr>
