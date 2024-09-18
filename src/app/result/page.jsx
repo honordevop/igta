db0e296f057b18e4848fd99783b1f0ee2b7c5166
unknown <ogunladestephen20@gmail.com>
2024-09-18 06:28:08 +0100

    Uploaded NGO Management Result and New Team member

--- src/app/result/page.jsx
+++ src/app/result/page.jsx
@@ -13,13 +13,10 @@ const Results = () => {
 
         <div className="container my-20 text-center flex flex-col gap-4 items-center justify-center">
           <h3 className="text-4xl font-bold ">
-            SUCCESSFUL CANDIDATES IN IGTA PEACE BUILDING AND CONFLICT MANAGEMENT
-            EXAMS OF JULY 2024.
+          SUCCESSFUL CANDIDATES IN IGTA NGO MANAGEMENT EXAMS OF SEPTEMBER 2024.
           </h3>
           <p className="text-xl">
-            518 Students Registered For the Training, 113 Participated In the
-            Exams/Presentations. 41 Students Succeeded with a Minimum Score of
-            50%. <br />
+          315 Students Registered For the Training, 98 Participated In the Examination. 69 Students Succeeded with a Minimum Score of 50%. <br />
             Congratulations !!!
           </p>
         </div>
@@ -27,11 +24,7 @@ const Results = () => {
         <div className="container">
           <ResultList />
           <p className="text-xl my-4 ">
-            Congratulations once more to all those who succeeded in the
-            examinations while we encourage those who didn’t succeed to try
-            again next year. Students are encouraged to forward all their
-            complaints directly to the management of IGTA International via the
-            organizations mailing addresses.
+          Congratulations once more to all those who succeeded in the examinations while we encourage those who didn’t succeed to try again next year. Students are encouraged to forward all their complaints directly to the management of IGTA International via any of the organizations mailing addresses.
           </p>
         </div>
       </div>
